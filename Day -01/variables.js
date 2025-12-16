ee912b90163af9e62175bee8f87c6fddddd4e01e
Karthikeyan <muthuvelkarthi5554@gmail.com>
2025-12-16 06:29:41 +0530

    Day 1 Morning Session complete: ran all JS fundamentals tasks individually

---
+++ Day -01/variables.js
@@ -0,0 +1,60 @@
+// let a = 10;
+
+// const b = 20;
+
+// if (true) {
+//   let a = 30;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+//Swap two variables without temp
+
+// let a = 20;
+// let b = 40;
+
+// // a = b;
+// // b = 20;
+
+// [a, b] = [b, a];
+
+// console.log('val of a', a);
+// console.log('val of b', b);
+//Destucturing Assignment
+
+
+//Check type of every JS value
+
+// const t1 = 'Alphabet';
+// const t2 = 2;
+// const t3 = true;
+// let t4;
+// const t5 = null;
+
+// console.log(typeof (t1));
+// console.log(typeof (t2));
+// console.log(typeof (t3));
+// console.log(typeof (t4));
+// console.log(typeof (t5));
+
+// Create an object and mutate it and going to try freeze it and mutate
+
+// const person = {
+//   Name: 'Karthikeyan',
+//   Age: 12,
+//   City: 'Chennai'
+// };
+// console.log('Object created ', person);
+// // Object.freeze(person);
+// person.Age = 23
+// person.Stack = { Frontend: 'Angular', Backend: 'Node & Mssql' }
+// console.log('Mutated val', person);
+
+
+
+// Use typeof on array (understand the lie)
+
+let arr = ['apple', 'Mango', 'cherry', 'Banana'];
+
+console.log(typeof (arr));
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 #  javascript-daily-practice
 Progress of My Daily Learning Javascript
+
+# JavaScript Learning Journey
+
+This repository documents my daily JavaScript practice.
+Each folder represents structured learning and hands-on coding.
+
+Start date: 16 Dec 2025
+Goal: Strong JS fundamentals → Angular mastery
